beb02bd046e628b5b48ca2d216c7362c47fe30a5
Rizky Sam Pratama <rizkysampratama@gmail.com>
2020-12-04 17:01:30 +0700

    fixed by eslint

--- src/utils.js
+++ src/utils.js
@@ -1,59 +1,33 @@
 const fs = require('fs');
 
-exports.sleep = ms => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+exports.sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-exports.readfile = async (directory) => {
-    return new Promise((resolve, reject) => {
-        try {
-            resolve(fs.readFileSync(directory, 'utf8'));
-        } catch (error) {
-            reject('error');
-        }
-    });
-};
+exports.readfile = async (directory) => new Promise((resolve, reject) => {
+  try {
+    resolve(fs.readFileSync(directory, 'utf8'));
+  } catch (error) {
+    reject(error);
+  }
+});
 
-exports.writefile = async (path, data) => {
-    return new Promise((resolve, reject) => {
-        try {
-            resolve(fs.writeFileSync(path, data));
-        } catch (error) {
-            reject(error);
-        }
-    });
-};
+exports.writefile = async (path, data) => new Promise((resolve, reject) => {
+  try {
+    resolve(fs.writeFileSync(path, data));
+  } catch (error) {
+    reject(error);
+  }
+});
 
-exports.appendfile = async (path, data) => {
-    return new Promise((resolve, reject) => {
-        try {
-            resolve(fs.appendFileSync(path, data));
-        } catch (error) {
-            reject(error);
-        }
-    });
-};
-
-exports.isEmptyObject = (obj) => {
-    for (var key in obj) {
-        if (obj.hasOwnProperty(key))
-            return false;
-    }
-    return true;
-}
+exports.appendfile = async (path, data) => new Promise((resolve, reject) => {
+  try {
+    resolve(fs.appendFileSync(path, data));
+  } catch (error) {
+    reject(error);
+  }
+});
 
 exports.removeEmptyOrNull = (array) => {
-    let result = array.filter(function (el) {
-        return el != null;
-    });
+  const result = array.filter((el) => el != null);
 
-    return result
-}
-
-exports.isEmptyObject = (obj) => {
-    for (var key in obj) {
-        if (obj.hasOwnProperty(key))
-            return false;
-    }
-    return true;
-}
+  return result;
+};
